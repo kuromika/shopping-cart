0ec5b8ed3dcbddc6716d17280b9525a59701c96e
mika-alpha <yuukeikioku@gmail.com>
2022-11-15 17:39:08 -0500

    Handle empty cart at cart page

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -6,20 +6,22 @@ const Cart = (props) => {
         <div className="cart">
             <h2>Your Cart</h2>
             <div className="items">
-                {props.items.map( (item) => 
-                    <CartItem
-                        key={item.id}
-                        img={item.image}
-                        desc={item.desc}
-                        name={item.title}
-                        price={item.price}
-                        id = {item.id}
-                        quantity={item.quantity}
-                        add={props.add}
-                        remove={props.rm}
-                    ></CartItem>)}
+                {props.items.length === 0 ? <h1> Add items to your cart</h1> :
+                    props.items.map( (item) => 
+                        <CartItem
+                            key={item.id}
+                            img={item.image}
+                            desc={item.desc}
+                            name={item.title}
+                            price={item.price}
+                            id = {item.id}
+                            quantity={item.quantity}
+                            add={props.add}
+                            remove={props.rm}
+                        ></CartItem>)
+                }
             </div>
-            
+
             <div className="checkout">
                 <div className="cart-total">
                     <span>Total: </span>
