990884da5377bd836b7a3f4a0634ec8d21439591
mika-alpha <yuukeikioku@gmail.com>
2022-11-15 15:17:22 -0500

    Fix header layout

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,6 @@ import React from "react";
 const Home = () => {
     return (
         <div className="home">
-            <h1>Tica·α</h1>
             <p>Our products, your style</p>
         </div>
     )
--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import Nav from "./Components/Nav";
+import './styles/style.css'
 
 const App = (props) => {
   
